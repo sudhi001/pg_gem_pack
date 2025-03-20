0d367f0c69d0a0f180e4e943155623b8275ca55b
Sudhi S <sudhis@live.com>
2025-03-20 13:32:24 +0530

    test fix

--- model/org_device_mapper.go
+++ model/org_device_mapper.go
@@ -5,6 +5,7 @@ type OrgDeviceMapper struct {
 	ID              uint   `gorm:"primaryKey;autoIncrement"` // Auto-incremented ID
 	OrgID           int    `gorm:"not null"`                 // Organization ID
 	AccessPointName string `gorm:"type:varchar(100);not null"`
+	AccessPointId   int    `gorm:"not null"`
 	AccessType      string `gorm:"type:varchar(50);not null"`
 	IS_GATE         bool   `gorm:"not null;default:false"` // Default value set to false
 }
