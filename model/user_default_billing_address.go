36f24dcd02831039b49f33ba7078bbfc277ee93a
Sudhi S <sudhis@live.com>
2025-07-01 19:29:30 +0530

    fixes

--- model/user_default_billing_address.go
+++ model/user_default_billing_address.go
@@ -4,6 +4,8 @@ package models
 type UserDefaultBillingAddress struct {
 	BaseModel
 	UserID              string  `gorm:"uniqueIndex;not null"` // Unique identifier for the user
+	FirstName           *string `gorm:"default:null"`
+	LastName            *string `gorm:"default:null"`
 	BuildingName        *string `gorm:"default:null"`
 	BuildingNumber      *string `gorm:"default:null"`
 	LocationName        *string `gorm:"default:null"`
